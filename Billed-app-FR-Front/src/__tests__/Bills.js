2df917afd777bb701471b2e117d408de76b42517
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-03-26 13:24:45 +0100

    prepare session n°2

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -7,6 +7,7 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 
 import Bills from '../containers/Bills.js';
+import store from '../app/Store';
 
 import { screen, waitFor } from '@testing-library/dom';
 import BillsUI from '../views/BillsUI.js';
@@ -14,51 +15,55 @@ import { bills } from '../fixtures/bills.js';
 import { ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
 import router from '../app/Router.js';
+import NewBillUI from '../views/NewBillUI.js';
 
 describe('Given I am connected as an employee', () => {
-	describe('When I am on Bills Page', () => {
-		test('Then bill icon in vertical layout should be highlighted', async () => {
-			Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-			window.localStorage.setItem(
-				'user',
-				JSON.stringify({
-					type: 'Employee',
-				})
-			);
-			const root = document.createElement('div');
-			root.setAttribute('id', 'root');
-			document.body.append(root);
-			router();
-			window.onNavigate(ROUTES_PATH.Bills);
-			await waitFor(() => screen.getByTestId('icon-window'));
-			const windowIcon = screen.getByTestId('icon-window');
-			// * Le routeur injecte la classe 'active-icon' quand l'utilisateur est
-			// * sur la page 'Bills'
-			expect(windowIcon).toHaveClass('active-icon');
-		});
+  describe('When I am on Bills Page', () => {
+    test('Then bill icon in vertical layout should be highlighted', async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee',
+        })
+      );
+      const root = document.createElement('div');
+      root.setAttribute('id', 'root');
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId('icon-window'));
+      const windowIcon = screen.getByTestId('icon-window');
+      // * Le routeur injecte la classe 'active-icon' quand l'utilisateur est
+      // * sur la page 'Bills'
+      expect(windowIcon).toHaveClass('active-icon');
+    });
 
-		test('Then bills should be ordered from earliest to latest', () => {
-			document.body.innerHTML = BillsUI({ data: bills });
-			const dates = screen
-				.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
-				.map((a) => a.innerHTML);
-			const antiChrono = (a, b) => (a < b ? 1 : -1);
-			const datesSorted = [...dates].sort(antiChrono);
-			expect(dates).toEqual(datesSorted);
-		});
+    test('Then bills should be ordered from earliest to latest', () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
 
-		// ********* Ligne 26 handleClickNewBill()
-		describe("When I click on button 'new bill'", () => {
-			const handleClickNewBill = jest.fn((e) => dashboard.handleAcceptSubmit(e, bills[0]));
-			test('Then we should be redirect to the new bill page', () => {
-				const html = BillsUI({ data: bills });
-				document.body.innerHTML = html;
-				const newBillButton = screen.getByTestId('btn-new-bill');
-				newBillButton.addEventListener('click', handleClickNewBill);
-				userEvent.click(newBillButton);
-				console.log(html);
-				expect();
-			});
-		});
-	});
+    // ********* Ligne 26 handleClickNewBill()
+    describe("When I click on button 'new bill'", () => {
+      const bills = new Bills({ document, router, store, localStorage });
+      const handleClickNewBill = jest.fn((e) => bills.handleClickNewBill());
+
+      test('Then we should be redirect to the new bill page', () => {
+        document.body.innerHTML = BillsUI({ data: bills });
+
+        const newBillButton = screen.getByTestId('btn-new-bill');
+
+        newBillButton.addEventListener('click', handleClickNewBill);
+        userEvent.click(newBillButton);
+
+        expect(screen.getByTestId('form-new-bill')).toBeTruthy();
+      });
+    });
+  });
 });
